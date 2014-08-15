acec38102412627d4ed59aca855154590d538523
zhengyusun <zhensn@gmail.com>
2014-08-14 19:05:27 -0700

    initial setup

--- public_html/canvasFilter.js
+++ public_html/canvasFilter.js
@@ -9,14 +9,34 @@ imageObj.onload = function() {
     canvas.height = imageObj.height;
     canvas.width = imageObj.width;
     context.drawImage(imageObj, 0, 0);
-    
+
     var imageData = context.getImageData(0, 0, imageObj.width, imageObj.height);
-    var data = imageData.data;
-    for (var i = 0; i < data.length; i += 4) {
-        data[i] = 255 - data[i];
-        data[i + 1] = 255 - data[i + 1];
-        data[i + 2] = 255 - data[i + 2];
+    
+    for (var i = 0; i < imageData.data.length; i += 4) {
+        imageData.data[i] = 255 - imageData.data[i];
+        imageData.data[i + 1] = 255 - imageData.data[i + 1];
+        imageData.data[i + 2] = 255 - imageData.data[i + 2];
     }
+    imageData.data = boxBlur(imageData.data, imageObj.width, imageObj.height, 10)
     context.putImageData(imageData, 0, 0);
 };
 
+function boxBlur(inputData, imageWidth, imageHeight, radius) {
+    var newData = inputData;
+    for(var i = 0; i < imageWidth; i++) {
+        for(var j = 0; j < imageHeight; j++) {
+            totalRed = 0;
+            totalGreen = 0;
+            totalBlue = 0;
+            for (var ki = -radius; ki <= radius; ki++) {
+                totalRed += inputData[((imageWidth * j + i)) * 4];
+                totalGreen += inputData[((imageWidth * j + i)) * 4 + 1];
+                totalBlue += inputData[((imageWidth * j + i)) * 4 + 2];
+            }
+            newData[((imageWidth * j + i)) * 4] = totalRed/(radius * 2 + 1);
+            newData[((imageWidth * j + i)) * 4 + 1] = totalGreen/(radius * 2 + 1);
+            newData[((imageWidth * j + i)) * 4 + 2] = totalBlue/(radius * 2 + 1);
+        }
+    }
+    
+}
